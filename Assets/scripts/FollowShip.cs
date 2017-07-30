10f73d266b5fa184d97483b2f51dc79130db93f8
sketchycode <jeremy@sketchyventures.com>
2017-07-30 13:19:13 -0600

    camera now correctly puts ship into center of viewport

--- Assets/scripts/FollowShip.cs
+++ Assets/scripts/FollowShip.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[ExecuteInEditMode]
 public class FollowShip : MonoBehaviour
 {
     public Transform followTarget;
-    public float followXOffset = 0f;
-    public float followYOffset = 0f;
+
+    public float uiSliceLeftBorder = 259f;
+    public float uiSliceRightBorder = 39;
+
+    private Camera cam;
 
     private void Start()
     {
         if(followTarget == null) { Debug.LogError("followTarget property must be set"); }
+        cam = GetComponent<Camera>();
     }
     // Update is called once per frame
     void Update()
     {
+        float sizeUnitsPerPixel = (cam.orthographicSize * 2) / Screen.height;
+        float xOffset = ((uiSliceLeftBorder - uiSliceRightBorder) / 2f) * sizeUnitsPerPixel;
         float curZ = transform.position.z;
         var targetPos = followTarget.position;
-        transform.position = new Vector3(targetPos.x + followXOffset, targetPos.y + followYOffset, curZ);
+        transform.position = new Vector3(targetPos.x - xOffset, targetPos.y, curZ);
     }
 }
