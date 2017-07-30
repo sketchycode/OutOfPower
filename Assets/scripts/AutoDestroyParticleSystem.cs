0a72ea62fb6cfe8fdc98f79e21576b884bb2e67f
sketchycode <jeremy@sketchyventures.com>
2017-07-30 14:44:55 -0600

    asteroids explode now, closes #7

---
+++ Assets/scripts/AutoDestroyParticleSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class AutoDestroyParticleSystem : MonoBehaviour
+{
+    private ParticleSystem effect;
+
+    // Use this for initialization
+    void Start()
+    {
+        effect = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!effect.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/scripts/level/Asteroid.cs
+++ Assets/scripts/level/Asteroid.cs
@@ -6,14 +6,28 @@ public class Asteroid : MonoBehaviour
 {
     public event EventHandler Destroyed;
 
+    public ParticleSystem explosionEffect;
+
     private void OnTriggerEnter2D(Collider2D collider)
+    {
+        HandleTriggerEntered(collider);
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        HandleTriggerEntered(collider);
+    }
+
+    private void HandleTriggerEntered(Collider2D collider)
     {
         if (collider.gameObject.tag == "Player")
         {
             var shields = collider.gameObject.GetComponent<Shields>();
-            if(shields.IsActivated)
+            if (shields.IsActivated)
             {
-                if(Destroyed != null)
+                var effect = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+                effect.Play();
+                if (Destroyed != null)
                 {
                     Destroyed(this, EventArgs.Empty);
                 }
