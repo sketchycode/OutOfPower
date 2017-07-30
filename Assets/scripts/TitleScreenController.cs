d6f2707866075b7e558e8ccc30faa7053648f47a
sketchycode <jeremy@sketchyventures.com>
2017-07-30 16:04:00 -0600

    added a title song, closes #13

--- Assets/scripts/TitleScreenController.cs
+++ Assets/scripts/TitleScreenController.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreenController : MonoBehaviour
 {
+    private void Start()
+    {
+        var audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+
     public void NewGameButtonClicked()
     {
         SceneManager.LoadScene(1);
